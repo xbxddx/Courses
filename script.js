1534bc548f61031d0b39b62e990866b49fb790d9
xbxddx <61392205+xbxddx@users.noreply.github.com>
2020-04-10 18:44:57 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+let money = prompt("Какой Ващ бюджет на месяц?", ""),
+    time = prompt("Введите Дату в формате YYYY-MM-DD", "");
+
+
+let appData = {
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    timeData: time,
+    savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце",""),
+    a2 = prompt("Во сколько обойдется?",""),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце",""),
+    a4 = prompt("Во сколько обойдется?","");
+
+appData.expenses[a1] = a2;
+appData.expenses[a3] = a4;
+
+alert(appData.budget/30);
+
+
+console.log()
+
+
+
+
